14cb3dc2395fdef57c00be7c30c6576f280a2983
Alexander Van Maele <alexander@greenisland.be>
2023-11-30 11:59:16 +0100

    🍱 Add favicon and webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,36 +3,86 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Release Management',
-			logo: {
-				src: '/src/assets/release-management-planets-transparent.png',
-				replacesTitle: true,
-			},
-			favicon: '/src/assets/release-management-planets-transparent.png',
-			social: {
-				github: 'https://github.com/atlesque/release-management-book',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: [
-				'./src/styles/custom.css',
-				// Custom fonts
-				'@fontsource-variable/inter',
-				'@fontsource/spectral',
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Release Management',
+      logo: {
+        src: '/src/assets/release-management-planets-transparent.png',
+        replacesTitle: true,
+      },
+      favicon: '/favicon.ico',
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: '/apple-touch-icon.png',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/favicon-32x32.png',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            href: '/favicon-16x16.png',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'manifest',
+            href: '/site.webmanifest',
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'msapplication-TileColor',
+            content: '#407099',
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'theme-color',
+            content: '#ffffff',
+          },
+        },
+      ],
+
+      social: {
+        github: 'https://github.com/atlesque/release-management-book',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+      customCss: [
+        './src/styles/custom.css',
+        // Custom fonts
+        '@fontsource-variable/inter',
+        '@fontsource/spectral',
+      ],
+    }),
+  ],
 });
