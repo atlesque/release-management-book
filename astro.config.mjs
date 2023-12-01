c789dde24c59af3d8d9dcba3ca7ab3809e5b50df
Alexander Van Maele <alexander@greenisland.be>
2023-12-01 10:37:20 +0100

    ✨ Add What and Why chapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -66,15 +66,8 @@ export default defineConfig({
       },
       sidebar: [
         {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: 'Introduction',
+          autogenerate: { directory: 'introduction' },
         },
       ],
       customCss: [
